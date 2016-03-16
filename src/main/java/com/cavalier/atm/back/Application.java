6840cebe55d3602b31d0d58316eb455cd0a6b370
Charlotte Cavalier <malika@localhost.localdomain>
2016-03-16 23:17:27 +0100

    init

--- src/main/java/com/cavalier/atm/back/Application.java
+++ src/main/java/com/cavalier/atm/back/Application.java
@@ -1,4 +1,4 @@
-package com.cavalier.questionnaire.back;
+package com.cavalier.atm.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
